70178d6dd6729443086f8764e9b5f8c419a8cdb3
SebastianBermudezGutierrez <sbermudez-2023a@corhuila.edu.co>
2025-11-11 10:17:02 +0900

    Add template and script setup to PointCard component

--- src/components/PointCard.tsx
+++ src/components/PointCard.tsx
@@ -1,1 +1,15 @@
+<template>
+  <ion-header>
+    <ion-toolbar color="warning">
+      <ion-buttons slot="start">
+        <ion-menu-button />
+      </ion-buttons>
+      <ion-title>{{ title }}</ion-title>
+    </ion-toolbar>
+  </ion-header>
+</template>
 
+<script setup>
+import { IonHeader, IonToolbar, IonTitle, IonButtons, IonMenuButton } from '@ionic/vue';
+defineProps({ title: String });
+</script>

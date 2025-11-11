3db4344a54f263e77272b509002fac60b9bf300e
SebastianBermudezGutierrez <sbermudez-2023a@corhuila.edu.co>
2025-11-11 10:01:50 +0900

    Update PointForm.tsx

--- src/components/PointForm.tsx
+++ src/components/PointForm.tsx
@@ -1,1 +1,21 @@
+<template>
+  <ion-menu content-id="main-content">
+    <ion-header>
+      <ion-toolbar color="warning">
+        <ion-title>Registro De Puntos</ion-title>
+      </ion-toolbar>
+    </ion-header>
 
+    <ion-content>
+      <ion-list>
+        <ion-item router-link="/tabs/training">Identificador unico</ion-item>
+        <ion-item router-link="/tabs/daily-log">Registro Diario</ion-item>
+        <ion-item router-link="/tabs/exercise">Historial Semanal</ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-menu>
+</template>
+
+<script setup>
+import { IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem } from '@ionic/vue';
+</script>

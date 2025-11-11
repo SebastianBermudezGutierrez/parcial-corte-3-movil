33908887e9db18af317e3c9af143fb39749b0417
SebastianBermudezGutierrez <sbermudez-2023a@corhuila.edu.co>
2025-11-11 10:07:05 +0900

    Update PointsListView.tsx

--- src/views/PointsListView.tsx
+++ src/views/PointsListView.tsx
@@ -1,1 +1,65 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar color="dark">
+        <ion-title>Registro De Puntos</ion-title>
+      </ion-toolbar>
+    </ion-header>
 
+    <ion-content class="ion-padding">
+      <ion-input
+        label="id"
+        label-placement="floating"
+        placeholder="Identificador único"
+      />
+      <ion-input
+        label="title"
+        label-placement="floating"
+        placeholder="Título del punto"
+      />
+      <ion-input
+        label="description"
+        label-placement="floating"
+        placeholder="Descripción detallada"
+      />
+      <ion-input
+        label="date"
+        label-placement="floating"
+        placeholder="Fecha de registro"
+      />
+      <ion-input
+        label="status"
+        label-placement="floating"
+        type="password"
+        placeholder="Estado del punto (activo/inactivo)"
+      />
+      <ion-button expand="block" router-link="/tabs/training" color="dark">
+        Entrar
+      </ion-button>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonInput,
+  IonButton
+} from '@ionic/vue'
+</script>
+
+<style scoped>
+ion-content {
+  background-color: black;
+  color: white;
+}
+
+ion-title {
+  text-align: center;
+  color: white;
+}
+</style>
